1091b708d75c7d3bcf304e0461f46c305f568494
Aidan Slate <slate.aidan@gmail.com>
2016-08-07 17:56:35 -0400

    movement and dropdown with menus

--- es6/main.js
+++ es6/main.js
@@ -15,7 +15,25 @@ class GameState extends Phaser.State {
 
   create() {
     let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-    let player = new Player(this.game, center.x, center.y, 'mushroom');
+    this.game.physics.startSystem(Phaser.Physics.Arcade);
+    this.player = new Player(this.game, center.x, center.y, 'mushroom');
+    this.game.physics.arcade.enable(this.player);
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.right.isDown) {
+      this.player.body.velocity.x += 10
+    }
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x -= 10
+    }
+    if (this.cursors.up.isDown) {
+      this.player.body.velocity.y -= 10
+    }
+    if (this.cursors.down.isDown) {
+      this.player.body.velocity.y += 10
+    }
   }
 }
 
--- public/javascript/bundle.js
+++ public/javascript/bundle.js
@@ -90,7 +90,26 @@
 	    key: 'create',
 	    value: function create() {
 	      var center = { x: this.game.world.centerX, y: this.game.world.centerY };
-	      var player = new Player(this.game, center.x, center.y, 'mushroom');
+	      this.game.physics.startSystem(Phaser.Physics.Arcade);
+	      this.player = new Player(this.game, center.x, center.y, 'mushroom');
+	      this.game.physics.arcade.enable(this.player);
+	      this.cursors = this.game.input.keyboard.createCursorKeys();
+	    }
+	  }, {
+	    key: 'update',
+	    value: function update() {
+	      if (this.cursors.right.isDown) {
+	        this.player.body.velocity.x += 10;
+	      }
+	      if (this.cursors.left.isDown) {
+	        this.player.body.velocity.x -= 10;
+	      }
+	      if (this.cursors.up.isDown) {
+	        this.player.body.velocity.y -= 10;
+	      }
+	      if (this.cursors.down.isDown) {
+	        this.player.body.velocity.y += 10;
+	      }
 	    }
 	  }]);
 	
