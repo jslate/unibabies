de33ac6347abf7993ab1ae77edfff5101471549e
Jonathan Slate <jslate@patientslikeme.com>
2016-08-08 13:10:39 -0400

    moving move logic to character class

--- es6/Scene.js
+++ es6/Scene.js
@@ -3,7 +3,7 @@ import SceneState from './SceneState'
 class Scene extends SceneState {
   create() {
     this.preCreate();
-    this.moveAndTalk([
+    this.player.moveAndTalk([
       {distance: 100, direction: 'right', message: 'I am here', pause: 1},
       {distance: 100, direction: 'left', message: 'I am there', pause: 1},
       {distance: 100, direction: 'up', message: 'I am...', pause: 1},
--- es6/SceneState.js
+++ es6/SceneState.js
@@ -18,38 +18,6 @@ class SceneState extends Phaser.State {
     this.game.physics.arcade.enable(this.player);
   }
 
-  moveAndTalk(array) {
-    let startAtTime = 0;
-    array.forEach((obj) => {
-      setTimeout(() => {
-        this.movePlayer(obj.distance, obj.direction, () => this.player.speak(obj.message));
-      }, startAtTime);
-      startAtTime += obj.distance * this.move_speed + obj.pause * 1000;
-    });
-
-  }
-
-  movePlayerRight(distance, callback) { this.movePlayer(distance, 'right', callback);  }
-  movePlayerLeft(distance, callback) { this.movePlayer(distance, 'left', callback);  }
-  movePlayerUp(distance, callback) { this.movePlayer(distance, 'up', callback);  }
-  movePlayerDown(distance, callback) { this.movePlayer(distance, 'down', callback);  }
-
-  movePlayer(distance, direction, callback) {
-    let i = 0;
-    let interval;
-    interval = setInterval(() => {
-      if (direction == 'right') { this.player.x += 1; }
-      if (direction == 'left') { this.player.x -= 1; }
-      if (direction == 'up') { this.player.y += 1; }
-      if (direction == 'down') { this.player.y -= 1; }
-      i++;
-      if (i >= distance) {
-        clearInterval(interval);
-        callback();
-      }
-    }, this.move_speed);
-  }
-
 }
 
 export default SceneState;
